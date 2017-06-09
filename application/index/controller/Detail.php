2b6dde08e5d6342f86082692ec55f3cd4760e483
mruswang <875484737@qq.com>
2017-06-09 16:19:25 +0800

    增加页面内容

---
+++ application/index/controller/Detail.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+class Detail extends Base
+{
+    public function index(){
+
+
+        return  $this->fetch('',[
+
+        ]);
+
+    }
+}
